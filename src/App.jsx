bf48e93a1a0c311a50e7942a05b2e461150c9ba5
anzerbakhsh <anzerbakhsh@gmail.com>
2024-11-23 15:36:48 +0500

    todo app using redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react"
+import AddTodo from "./components/Addtodo"
+import Todos from "./components/Todos"
+
 
 function App() {
 
   return (
     <>
-   <h1>hello redux</h1>
+    <h1 className="text-red-600">hello</h1>
+   <AddTodo/>
+   <Todos/>
     </>
   )
 }
